6af53a83ce122c097f56412e82448065ea19e137
albankerloch <albankerloch@gmail.com>
2020-04-08 02:10:20 +0200

    ajout ssh

--- srcs/nginx/entrypoint.sh
+++ srcs/nginx/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+mv -f /sshd_config /etc/ssh/sshd_config
+mkdir .ssh
+mv /authorized_keys .ssh/authorized_keys
+chmod 600 /etc/ssh/ssh_host_rsa_key
+chmod 700 .ssh
+chmod 644 .ssh/authorized_keys
+
 adduser -D "$SSH_USER"
 echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+echo "user:password = $SSH_USER:$SSH_PASSWORD"
+
 /usr/sbin/sshd
+
 /usr/sbin/nginx -g 'daemon off;'
