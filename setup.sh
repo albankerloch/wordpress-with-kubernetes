bb65fab5da4f0dcb1f69c1ebfd00de50ddf50540
albankerloch <albankerloch@gmail.com>
2020-04-11 11:55:37 +0200

    new ssh keys

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-35000
+
+minikube addons enable ingress
+
 eval $(minikube docker-env)
 
 docker build -t nginxalban srcs/nginx
