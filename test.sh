fc561cee3ced09829061a2390c4cb41860b53d63
albankerloch <alban.kerloch@gmail.com>
2020-04-12 03:28:23 +0200

    grafana/grafana

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export IP_MINIKUBE=$(minikube ip)
-
-cp srcs/yaml/config/template-telegraf.conf srcs/yaml/config/telegraf.conf
-
-kubectl apply -f srcs/yaml/nginx-deployment.yaml
-kubectl apply -f srcs/yaml/nginx-service.yaml
