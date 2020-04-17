41157c17dd56c5a93ecf7d461d6cbf9bd805ac7a
albankerloch <alban.kerloch@gmail.com>
2020-04-17 22:04:02 +0200

    fix ftps

--- essai.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export MINI_IP=$(minikube ip)
-
-echo "bravo"
-
- echo $MINI_IP
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export MINI_IP=$(minikube ip)
-
-cp srcs/mysql/template_wordpress.sql srcs/mysql/wordpress.sql
-sed -i s/__MINI_IP__/$(minikube ip)/g srcs/mysql/wordpress.sql
